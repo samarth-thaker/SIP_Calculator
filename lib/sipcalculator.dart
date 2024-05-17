ef3c4551ebadfd57d6540a6bdd33f31e7048c1c1
samarth-thaker <samarth.thaker07@gmail.com>
2024-05-17 16:14:00 +0530

    Updated Flutter SIP Calculator

--- lib/sipcalculator.dart
+++ lib/sipcalculator.dart
@@ -9,27 +9,54 @@ class SIPscreen extends StatefulWidget {
 }
 
 class _SIPscreenState extends State<SIPscreen> {
-  final TextEditingController _monthlyInvestmentController = TextEditingController();
-  final TextEditingController _annualInterestRateController = TextEditingController();
+  final TextEditingController _monthlyInvestmentController =
+      TextEditingController();
+  final TextEditingController _annualInterestRateController =
+      TextEditingController();
   final TextEditingController _yearsController = TextEditingController();
+  //final TextEditingController _amountInvestedController = TextEditingController();
 
   double _maturityValue = 0.0;
+  double _amountInvested = 0.0;
+  double _earnings = 0.0;
 
-  double calculateSIPMaturity(double monthlyInvestment, double annualInterestRate, int years) {
-    double monthlyRate = (annualInterestRate / 100) / 12; // Monthly interest rate
+  double calculateSIPMaturity(
+      double monthlyInvestment, double annualInterestRate, int years) {
+    double monthlyRate =
+        (annualInterestRate / 100) / 12; // Monthly interest rate
     int months = years * 12; // Total number of months
 
-    double maturityValue = monthlyInvestment * (pow(1 + monthlyRate, months) - 1) / monthlyRate * (1 + monthlyRate);
+    double maturityValue = monthlyInvestment *
+        (pow(1 + monthlyRate, months) - 1) /
+        monthlyRate *
+        (1 + monthlyRate);
     return maturityValue;
+  }
+
+  double investedAmount(
+      double monthlyInvestment, double annualInterestRate, int years) {
+    double amountInvested = monthlyInvestment * years * 12;
+    return amountInvested;
+  }
+
+  double amountEarned(
+      double monthlyInvestment, double annualInterestRate, int years) {
+    return calculateSIPMaturity(monthlyInvestment, annualInterestRate, years) -
+        investedAmount(monthlyInvestment, annualInterestRate, years);
   }
 
   void _calculate() {
     double monthlyInvestment = double.parse(_monthlyInvestmentController.text);
-    double annualInterestRate = double.parse(_annualInterestRateController.text);
+    double annualInterestRate =
+        double.parse(_annualInterestRateController.text);
     int years = int.parse(_yearsController.text);
 
     setState(() {
-      _maturityValue = calculateSIPMaturity(monthlyInvestment, annualInterestRate, years);
+      _maturityValue =
+          calculateSIPMaturity(monthlyInvestment, annualInterestRate, years);
+      _amountInvested =
+          investedAmount(monthlyInvestment, annualInterestRate, years);
+      _earnings = amountEarned(monthlyInvestment, annualInterestRate, years);
     });
   }
 
@@ -51,7 +78,7 @@ class _SIPscreenState extends State<SIPscreen> {
                       color: Colors.deepOrange,
                     ),
                   ),
-                  hintText: "Investment per month",
+                  hintText: "Investment per month (in Rs.)",
                   contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
                 ),
                 keyboardType: TextInputType.number,
@@ -68,7 +95,7 @@ class _SIPscreenState extends State<SIPscreen> {
                       color: Colors.deepOrange,
                     ),
                   ),
-                  hintText: "Expected return",
+                  hintText: "Expected return (in % p.a)",
                   contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
                 ),
                 keyboardType: TextInputType.number,
@@ -85,7 +112,7 @@ class _SIPscreenState extends State<SIPscreen> {
                       color: Colors.deepOrange,
                     ),
                   ),
-                  hintText: "Time period (years)",
+                  hintText: "Time period (upto 50 years)",
                   contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
                 ),
                 keyboardType: TextInputType.number,
@@ -94,10 +121,14 @@ class _SIPscreenState extends State<SIPscreen> {
             const SizedBox(height: 30),
             TextButton(
               onPressed: _calculate,
-              child: const Text("Calculate"),
+              child: const Text("Calculate my wealth"),
             ),
             const SizedBox(height: 30),
-            Text('Maturity Value: $_maturityValue'),
+            Text('Maturity value: Rs. ${_maturityValue.toStringAsFixed(2)}'),
+            const SizedBox(height: 30),
+            Text('Amount invested: Rs. ${_amountInvested.toStringAsFixed(2)}'),
+            const SizedBox(height: 30),
+            Text('Earnings: Rs. ${_earnings.toStringAsFixed(2)}'),
           ],
         ),
       ),
